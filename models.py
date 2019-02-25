8038b2a03ad4e7026a15a62ec8ad45030c840682
Chao Ye <Chao12.ye@gmail.com>
2019-02-25 11:52:39 -0800

    removed venv

--- models.py
+++ models.py
@@ -9,3 +9,12 @@ def connect_db(app):
         """ connect to db"""
         db.app = app
         db.init_app(app)
+
+Class Cupcakes(db.Model):
+    ''' SQLALChemy Cupcakes class'''
+    __tablename__ = 'cupcakes'
+    id = db.Column()
+    #flavor
+    #size
+    #rating
+    #image
