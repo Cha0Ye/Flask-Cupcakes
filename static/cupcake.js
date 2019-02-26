f76cad6791adc5ce60c8f9ad27f3bba4636f2f52
Stéphane Dryefus <floatingrock@gmail.com>
2019-02-25 17:53:44 -0800

    Added AJAX call for show all.

--- static/cupcake.js
+++ static/cupcake.js
@@ -5,16 +5,27 @@ async function getAllCupcakes(){
     return response;
 }
 
+async function addCupcake(){
+    await $.post('http://localhost:5000/cupcakes',{flavor:''})
+}
+
 
 //wait for DOM to load
 $(document).ready(async function(e){
-    let {response} = await getAllCupcakes();
+    preventDefault(e);
 
+   
+    let $addCupCake = $('#add-cupcake');
     let $listItem = $("#cupcake-list");
-    for (let cupcake of response){
-        let newList = `<li> Flavor: ${cupcake.flavor} Size: ${cupcake.size} Rating: ${cupcake.rating} <img src='${cupcake.image}' width='100px'></li>`;
-        
-        $listItem.append(newList);
-    }
     
+    
+    
+    async function updateCupcakeList(){
+        let {response} = await getAllCupcakes();
+        for (let cupcake of response){
+            let newList = `<li> Flavor: ${cupcake.flavor} Size: ${cupcake.size} Rating: ${cupcake.rating} <img src='${cupcake.image}' width='100px'></li>`;
+            
+            $listItem.append(newList);
+        }
+    }
 })
